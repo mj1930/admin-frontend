9cf8185719dd7ac134e86593e0a6065aa32739dc
Mihir Jain <mj3003@Mihirs-MacBook-Air.local>
2021-05-16 13:14:52 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://6arhtir1m7.execute-api.ap-south-1.amazonaws.com/',
-  sellerApiUrl: 'https://abrwb40304.execute-api.ap-south-1.amazonaws.com/'
+  apiUrl: 'http://65.2.31.91:3002/',
+  sellerApiUrl: 'http://65.2.73.90:3003/'
 };
